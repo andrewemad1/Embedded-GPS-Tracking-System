820ea876306eeec913b7883273cd96adff3e4ab7
AndrewEmil22 <andrewemil22@gmail.com>
2021-06-04 21:39:44 +0200

    Initializing Port F pins

--- main.c
+++ main.c
@@ -3,7 +3,21 @@
 #include<string.h>
 #include<string.h>
 #include <Math.h>
+
 void SystemInit(){}
+void PORTFinit(){
+  volatile uint32_t delay;
+	SYSCTL_RCGCGPIO_R|=0x20;
+	delay=1;
+	GPIO_PORTF_LOCK_R=0x4C4F434B;
+	GPIO_PORTF_CR_R=0x1F;
+	GPIO_PORTF_AFSEL_R=0;
+	GPIO_PORTF_PCTL_R=0;
+	GPIO_PORTF_AMSEL_R=0;
+	GPIO_PORTF_DIR_R=0x0E;
+	GPIO_PORTF_DEN_R=0x1F;
+	GPIO_PORTF_PUR_R=0x11;
+}
 
 const int R = 6371; //Radius of the earth in (kilometers)
 const double PI=3.14159265358979323846; //Cont PI
