aea23e85932bdf11e38092cb79afa35bdcaf58d9
Andrew Emad <andre@HP-17.andrew>
2021-06-04 19:50:28 +0200

    Added distance code in main

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+double distance(double lat1, double lon1, double lat2, double lon2){
 
+  double theta1 = deg2rad(lat1);
+  double theta2 = deg2rad(lat2);
+  double delta1 = deg2rad(lat2 - lat1);
+  double delta2 = deg2rad(lon2 - lon1);
+
+  double a = sin(delta1 / 2) * sin(delta1 / 2) + cos(theta1) * cos(theta2) * sin(delta2 / 2) * sin(delta2 / 2);
+  double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+  double d = R * c;
+
+  return totalDis += d;
+}
