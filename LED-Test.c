7949a5b358800ee1f4d148a1cdff91c621aac06f
andrewemad1 <andrew_emad999@hotmail.com>
2021-06-05 02:32:11 +0200

    removed non required lines for the LED test

--- LED-Test.c
+++ LED-Test.c
@@ -2,10 +2,7 @@
 #include <stdlib.h>
 #include<string.h>
 #include <Math.h>
-#include "tm4c123gh6pm.h" 
-const int R = 6371; //Radius of the earth in (kilometers)
-const double PI=3.14159265358979323846; //Cont PI
-double totalDis = 0; //The total distance taken by the global variable
+#include "tm4c123gh6pm.h"
 
 void SystemInit(){}
 void PORTFinit(){
@@ -22,37 +19,19 @@ void PORTFinit(){
 	GPIO_PORTF_PUR_R=0x11;
 }
 
-
-double deg2rad(double deg){
-  return (deg * PI / 180);
-}
-
-double distance(double lat1, double lon1, double lat2, double lon2){
-
-  double theta1 = deg2rad(lat1);
-  double theta2 = deg2rad(lat2);
-  double delta1 = deg2rad(lat2 - lat1);
-  double delta2 = deg2rad(lon2 - lon1);
-
-  double a = sin(delta1 / 2) * sin(delta1 / 2) + cos(theta1) * cos(theta2) * sin(delta2 / 2) * sin(delta2 / 2);
-  double c = 2 * atan2(sqrt(a), sqrt(1 - a));
-  double d = R * c;
-
-  return totalDis += d;
-}
 int main(){
   PORTFinit();
 	while(1){
-	if((GPIO_PORTF_DATA_R&0x11)==0x10){    // blue light 
+	if((GPIO_PORTF_DATA_R&0x11)==0x10){    // blue light
 	GPIO_PORTF_DATA_R=0x04;
 	}
 	else if((GPIO_PORTF_DATA_R&0x11)==0x01){ // green light
 	GPIO_PORTF_DATA_R=0x08;
 	}
-	else if((GPIO_PORTF_DATA_R&0x11)==0x00){ // Red light 
+	else if((GPIO_PORTF_DATA_R&0x11)==0x00){ // Red light
 	GPIO_PORTF_DATA_R=0x02;
 	}
 	else GPIO_PORTF_DATA_R=0;      // No light
-	
+
 	}
 }
