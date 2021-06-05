7fbb0adf7b4a23d5e24b6a0af78b8941c99287eb
andrewemad1 <andrew_emad999@hotmail.com>
2021-06-05 16:23:49 +0200

    Added Harvesine Formula to calculate the distance between two points

--- main.c
+++ main.c
@@ -26,13 +26,31 @@ const int R = 6371; //Radius of the earth in (kilometers)
 const double PI=3.14159265358979323846; //Constant PI
 double totalDis = 0; //The total distance taken by the global variable
 
+
+//Function that calculates the total taken distance:
+
 //Function to convert from degree to radian
 double deg2rad(double deg){
   return (deg * PI / 180);
 }
 
+//Harvesine Formula to calculate the distance between two points
+double distance(double lat1, double lon1, double lat2, double lon2){
+
+  double theta1 = deg2rad(lat1);
+  double theta2 = deg2rad(lat2);
+  double delta1 = deg2rad(lat2 - lat1);
+  double delta2 = deg2rad(lon2 - lon1);
+
+  double a = sin(delta1 / 2) * sin(delta1 / 2) + cos(theta1) * cos(theta2) * sin(delta2 / 2) * sin(delta2 / 2);
+  double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+  double d = R * c;
+
+  return totalDis += d;
+}
 
 
+//function that turns on the LED when the distance exceeds 100 meters.
 bool FinalDestination(int distance)
 { if (distance>100)
     return true;
