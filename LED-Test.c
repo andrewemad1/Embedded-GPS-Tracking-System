a5b6ae7f910ba2597060c59ce54e3350d7466fb7
andrewemad1 <andrew_emad999@hotmail.com>
2021-06-05 22:59:45 +0200

    Fixed the startup code

--- LED-Test.c
+++ LED-Test.c
@@ -1,10 +1,9 @@
-#include "stdio.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
 #include <Math.h>
 #include "tm4c123gh6pm.h"
 
-void SystemInit(){}
 void PORTFinit(){
   volatile uint32_t delay;
 	SYSCTL_RCGCGPIO_R|=0x20;
