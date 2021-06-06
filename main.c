90be671560a328edf122d26fdc55baf35ad89e0d
Marconan04 <marco__hanna11@hotmail.com>
2021-06-06 14:20:25 +0200

    rebuild the LED distance function

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
+#include "stdio.h"
 #include <stdlib.h>
-#include <string.h>
-#include <inttypes.h>
-#include <math.h>
-#include <stdbool.h>
 #include "tm4c123gh6pm.h"
+#include <string.h>
+#include <Math.h>
+#include "std_types.h"
+#include "lcd.h"
 
+const int R = 6371; //Radius of the earth in (kilometers)
+const double PI=3.14159265358979323846; //Constant PI
+double totalDis = 0; //The total distance taken by the global variable
 
 void PORTFinit(){
   volatile uint32_t delay;
@@ -20,11 +23,6 @@ void PORTFinit(){
 	GPIO_PORTF_DEN_R=0x1F;
 	GPIO_PORTF_PUR_R=0x11;
 }
-
-const int R = 6371; //Radius of the earth in (kilometers)
-const double PI=3.14159265358979323846; //Constant PI
-double totalDis = 0; //The total distance taken by the global variable
-
 
 //Function that calculates the total taken distance:
 
@@ -50,23 +48,12 @@ double distance(double lat1, double lon1, double lat2, double lon2){
 
 
 //function that turns on the LED when the distance exceeds 100 meters.
-bool FinalDestination(int distance)
-{ if (distance>100)
-    return true;
-    else
-    return false;
+void func1(float x){
+
+if(x>=100)  GPIO_PORTF_DATA_R|=0x02 ;
+else   GPIO_PORTF_DATA_R &=~0x02;	
 }
 
-void LED_ON(int distance)
-{
-
-if(FinalDestination(distance))
-
-    GPIO_PORTF_DATA_R |= 0x02; //red
-else
-
-GPIO_PORTF_DATA_R &=~(0x02); //turns on red LED if distance >100
-}
 void PortEB_Init(){
 	// Initializing Clock and wait until get stablized
 	SYSCTL_RCGCGPIO_R |= 0x02;
