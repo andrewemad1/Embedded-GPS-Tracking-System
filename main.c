52419f5c0f387931db2544d0f9cc8d343ec4ad88
AntonyOssama <tony.ossama@gmail.com>
2021-06-13 14:38:29 +0200

    UART init function

--- main.c
+++ main.c
@@ -94,6 +94,24 @@ void Receive_GPS_Data(){
 			finished = 0;
    pos_cnt = 0;
 }
+void UART_Init(void) {
+    SYSCTL_RCGCUART_R |= 0x0004; //enable uart4 and Port D
+    SYSCTL_RCGCGPIO_R |= 0x0008;
+    UART2_CTL_R &= ~0x0001; //diable uart uartctl
+    //in uart0 use it as alternate function afsel
+    //pctl of uart
+
+    UART2_IBRD_R = 104;    //or 104 
+    UART2_FBRD_R = 11;   //baudrate
+    UART2_LCRH_R = 0x0070;      //uartlcrh: data lengh 8bit bit5,bit6 ==1 --one stop bit bit3 ==0 -- no parity bit1,bit2 ==0 --enable fifo bit4 ==1
+    UART2_CTL_R |= 0x0001;         //enable uartctl
+
+    GPIO_PORTD_AFSEL_R |= 0xC0;
+    GPIO_PORTD_PCTL_R = (GPIO_PORTD_PCTL_R & 0x00FFFFFF) + 0x11000000;
+    GPIO_PORTD_DEN_R |= 0xC0;      //digital enable
+    GPIO_PORTD_AMSEL_R &= ~0XC0;       //analog
+
+}
 //Function that calculates the total taken distance:
 
 //Function to convert from degree to radian
